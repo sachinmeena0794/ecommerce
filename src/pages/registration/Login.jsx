fa15d82c288d78a96acf8ad26d359c51936252fd
sachinmeena0794 <sachin.meena@indianic.com>
2024-02-11 16:01:12 +0530

    login

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,18 +1,20 @@
-import { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import myContext from '../../context/data/myContext'
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import myContext from '../../context/data/myContext';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../fireabase/FirebaseConfig';
 import { toast } from 'react-toastify';
 import Loader from '../../components/loader/Loader';
 import Layout from '../../components/layout/Layout';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function Login() {
-    const context = useContext(myContext)
-    const {loading, setLoading} = context;
+    const context = useContext(myContext);
+    const { loading, setLoading } = context;
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
 
     const navigate = useNavigate();
 
@@ -31,7 +33,7 @@ function Login() {
                 theme: "colored",
             });
             localStorage.setItem('user', JSON.stringify(result));
-            console.log(JSON.stringify(result))
+            console.log(JSON.stringify(result));
             navigate('/');
         } catch (error) {
             console.log(error);
@@ -48,55 +50,61 @@ function Login() {
             });
         }
     }
-    
-   
+
     return (
         <Layout>
- <div className='flex justify-center items-center h-screen'>
-          
-            <div className='bg-gray-800 px-10 py-16 rounded-xl w-full max-w-md' style={{ backgroundColor: '#908a81' }}>
-                <div>
-                    <h1 className='text-center text-white text-xl mb-6 font-bold'>Login</h1>
-                </div>
-                <div>
-                    <input 
-                        type="email"
-                        value={email}
-                        onChange={(e)=> setEmail(e.target.value)}
-                        name='email'
-                        className='bg-gray-600 mb-6 px-4 py-3 w-full rounded-lg text-black placeholder:text-black-200 outline-none'
-                        placeholder='Email'
-                        style={{ backgroundColor: '#e2e0dc' }}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
-                        className='bg-gray-600 mb-6 px-4 py-3 w-full rounded-lg text-black placeholder:text-black-200 outline-none'
-                        placeholder='Password'
-                        style={{ backgroundColor: ' #e2e0dc' }}
-                    />
-                </div>
-                <div className='flex justify-center mb-6'>
-                    <button
-                        onClick={login}
-                        className='bg-black w-full text-white font-bold px-4 py-3 rounded-lg'>
-                        Login
-                    </button>
-                </div>
-                <div>
-                    <h2 className='text-white text-center'>Don't have an account <Link className='text-black font-bold' 
-    style={{ transition: 'transform 0.3s ease-in-out', display: 'inline-block' }}
-    onMouseEnter={(e) => { e.target.style.transform = 'scale(1.15)'; }}
-    onMouseLeave={(e) => { e.target.style.transform = 'scale(1)'; }} to={'/signup'}>Signup</Link></h2>
+            <div className='flex justify-center items-center h-screen'>
+                <div className='bg-gray-800 px-10 py-16 rounded-xl w-full max-w-md' style={{ backgroundColor: '#908a81' }}>
+                    <div>
+                        <h1 className='text-center text-white text-xl mb-6 font-bold'>Login</h1>
+                    </div>
+                    <div>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            name='email'
+                            className='bg-gray-600 mb-6 px-4 py-3 w-full rounded-lg text-black placeholder:text-black-200 outline-none'
+                            placeholder='Email'
+                            style={{ backgroundColor: '#e2e0dc' }}
+                        />
+                    </div>
+                    <div>
+                        <div className='relative'>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='bg-gray-600 mb-6 px-4 py-3 w-full rounded-lg text-black placeholder:text-black-200 outline-none pr-10' // Added paddingRight to accommodate eye icon
+                                placeholder='Password'
+                                style={{ backgroundColor: '#e2e0dc' }}
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPassword(!showPassword)}
+                                className='absolute inset-y-0 right-0 px-4 mb-3 text-black rounded-r-lg focus:outline-none flex items-center justify-center'
+                            >
+                                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </button>
+                        </div>
+                    </div>
+                    <div className='flex justify-center mb-6'>
+                        <button
+                            onClick={login}
+                            className='bg-black w-full text-white font-bold px-4 py-3 rounded-lg'>
+                            Login
+                        </button>
+                    </div>
+                    <div>
+                        <h2 className='text-white text-center text-lg'>Don't have an account yet?<Link className='text-black font-bold px-1'
+                            style={{ transition: 'transform 0.3s ease-in-out', display: 'inline-block' }}
+                            onMouseEnter={(e) => { e.target.style.transform = 'scale(1.15)'; }}
+                            onMouseLeave={(e) => { e.target.style.transform = 'scale(1)'; }} to={'/signup'}>Signup</Link></h2>
+                    </div>
                 </div>
             </div>
-        </div>
         </Layout>
-       
     )
 }
 
-export default Login
+export default Login;
--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import myContext from '../../context/data/myContext';
 import { toast } from 'react-toastify';
@@ -7,11 +7,13 @@ import { auth, fireDB } from '../../fireabase/FirebaseConfig';
 import { Timestamp, addDoc, collection } from 'firebase/firestore';
 import Loader from '../../components/loader/Loader';
 import Layout from '../../components/layout/Layout';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function Signup() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
     const navigate = useNavigate(); // Initialize useNavigate
 
     const context = useContext(myContext);
@@ -50,60 +52,68 @@ function Signup() {
 
     return (
         <Layout>
-             <div className='flex justify-center items-center h-screen'>
-            {loading && <Loader/>}
-            <div className='bg-gray-800 px-10 py-16 rounded-xl w-full max-w-md' style={{ backgroundColor: '#908a81' }}>
-                <div className="">
-                    <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
-                </div>
-                <div>
-                    <input type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        name='name'
-                        className='bg-gray-600 mb-4 px-2 py-2 w-full rounded-lg text-black placeholder:text-black-200'
-                        placeholder='Name'
-                        style={{ backgroundColor: '#e2e0dc' }}
-                    />
-                </div>
+            <div className='flex justify-center items-center h-screen'>
+                {loading && <Loader/>}
+                <div className='bg-gray-800 px-10 py-16 rounded-xl w-full max-w-md' style={{ backgroundColor: '#908a81' }}>
+                    <div className="">
+                        <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
+                    </div>
+                    <div>
+                        <input type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            name='name'
+                            className='bg-gray-600 mb-4 px-2 py-2 w-full rounded-lg text-black placeholder:text-black-200'
+                            placeholder='Name'
+                            style={{ backgroundColor: '#e2e0dc' }}
+                        />
+                    </div>
 
-                <div>
-                    <input type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        name='email'
-                        className='bg-gray-600 mb-4 px-2 py-2 w-full rounded-lg text-black placeholder:text-black-200'
-                        placeholder='Email'
-                        style={{ backgroundColor: '#e2e0dc' }}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='bg-gray-600 mb-4 px-2 py-2 w-full rounded-lg text-black placeholder:text-black-200'
-                        placeholder='Password'
-                        style={{ backgroundColor: '#e2e0dc' }}
-                    />
-                </div>
-                <div className='flex justify-center mb-3'>
-                    <button
-                        onClick={signup}
-                        className='bg-black w-full text-white font-bold px-2 py-2 rounded-lg'>
-                        Signup
-                    </button>
-                </div>
-                <div>
-                    <h2 className='text-white'>Have an account <Link className='text-black font-bold' 
-    style={{ transition: 'transform 0.3s ease-in-out', display: 'inline-block' }}
-    onMouseEnter={(e) => { e.target.style.transform = 'scale(1.15)'; }}
-    onMouseLeave={(e) => { e.target.style.transform = 'scale(1)'; }} to={'/login'}>Login</Link></h2>
+                    <div>
+                        <input type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            name='email'
+                            className='bg-gray-600 mb-4 px-2 py-2 w-full rounded-lg text-black placeholder:text-black-200'
+                            placeholder='Email'
+                            style={{ backgroundColor: '#e2e0dc' }}
+                        />
+                    </div>
+                    <div>
+                        <div className='relative'>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='bg-gray-600 mb-4 px-2 py-2 w-full rounded-lg text-black placeholder:text-black-200'
+                                placeholder='Password'
+                                style={{ backgroundColor: '#e2e0dc' }}
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPassword(!showPassword)}
+                                className='absolute inset-y-0 right-0 px-4 mb-3 text-black rounded-r-lg focus:outline-none flex items-center justify-center'
+                            >
+                                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </button>
+                        </div>
+                    </div>
+                    <div className='flex justify-center mb-3'>
+                        <button
+                            onClick={signup}
+                            className='bg-black w-full text-white font-bold px-2 py-2 rounded-lg'>
+                            Signup
+                        </button>
+                    </div>
+                    <div>
+                        <h2 className='text-white text-lg'>Already have an account? <Link className='text-black font-bold px-1' 
+                            style={{ transition: 'transform 0.3s ease-in-out', display: 'inline-block' }}
+                            onMouseEnter={(e) => { e.target.style.transform = 'scale(1.15)'; }}
+                            onMouseLeave={(e) => { e.target.style.transform = 'scale(1)'; }} to={'/login'}>Login</Link></h2>
+                    </div>
                 </div>
             </div>
-        </div>
         </Layout>
-       
     )
 }
 
